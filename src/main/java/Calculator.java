50ad2199257cc520560bd1cb6383c15d2799887e
Yury <sav1orxp@gmail.com>
2021-01-27 20:15:00 +0300

    The End.

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,13 +6,38 @@ public class Calculator {
 
     static Scanner scan = new Scanner(System.in);
 
+    static int test () throws DivideZeroException {
+
+       int calculate;
+
+        String str = scan.nextLine();//Принимает на ввод строку
+
+        String operator = str.replaceAll("\\b\\d+\\b", "").trim();
+
+        int operand = str.indexOf(operator);
+
+        String afterOp = str.substring(operand + 1).replaceAll(" ", "");
+
+        String beforeOp = str.substring(0, operand).replaceAll(" ", "");
+        ;//Возвращает нам строку до знака операции
+
+        Integer rightOperand = Integer.valueOf(afterOp);
+
+        Integer leftOperand = Integer.valueOf(beforeOp);
+
+        return calculate(rightOperand,leftOperand,operator);
+    }
+
+
     public static int getInt() {
         System.out.println("Введите число: ");
+
         int num;
         if (scan.hasNextInt()) {
             num = scan.nextInt();
         } else {
             System.out.println("Вы допустили ошибку, попробуйте еще раз.");
+
             scan.next();
             num = getInt();
 
@@ -22,39 +47,44 @@ public class Calculator {
 
     public static char getOperation() {
         System.out.print("Введите операцию: ");
+
         char operation;
+
         if (scan.hasNext()) {
             operation = scan.next().charAt(0);
         } else {
             System.out.println("Вы допустили ошибку, повторите ввод.");
             scan.next();
+
             operation = getOperation();
 
         }
         return operation;
     }
 
-    public static int calculate(int num1, int num2, char operation) throws DivideZeroException {
+    public static int calculate(int num1, int num2, String operation) throws DivideZeroException {
         int result;
         switch (operation) {
-            case '+':
+            case "+":
                 result = num1 + num2;
+
                 break;
 
-            case '-':
+            case "-":
                 result = num1 - num2;
                 break;
 
-            case '*':
+            case "*":
                 result = num1 * num2;
                 break;
 
-            case '/':
+            case "/":
                 if (num2 == 0) {
                     throw new DivideZeroException();
                 }
                 result = num1 / num2;
                 break;
+
             default:
                 System.out.println("Ввод был не верный, повторите еще раз.");
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,8 @@ package main.java;
 
 public class Main {
     public static void main(String[] args) throws DivideZeroException {
-        int num1 = Calculator.getInt();
-        int num2 = Calculator.getInt();
-        char operation = Calculator.getOperation();
-        int result = Calculator.calculate(num1, num2, operation);
-        System.out.println("Результат операции: " + result);
+        Calculator fin = new Calculator();
+
+        System.out.println("Результат операции: " + fin.test());
     }
 }
